0a309cad92ccda30a4e442370c7f9ff959402ca9
Chaeyoung Song <112750856+cy0286@users.noreply.github.com>
2024-12-17 16:49:24 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -0,0 +1,11 @@
+def solution(a, b):
+    answer = 0
+    
+    if int(str(a) + str(b)) > int(str(b) + str(a)):
+        answer = int(str(a) + str(b))
+    elif int(str(a) + str(b)) < int(str(b) + str(a)):
+        answer = int(str(b) + str(a))
+    else:
+        answer = int(str(a) + str(b))
+    
+    return answer
