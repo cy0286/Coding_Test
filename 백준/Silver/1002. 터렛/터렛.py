1998f582fb8ee17babafbab08802fda7bacbe6d3
Chaeyoung Song <112750856+cy0286@users.noreply.github.com>
2024-12-18 00:22:19 +0900

    [Silver III] Title: 터렛, Time: 44 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Silver/1002. 터렛/터렛.py
+++ 백준/Silver/1002. 터렛/터렛.py
@@ -8,29 +8,16 @@ for _ in range(T):
     dist_y = (y2 - y1) ** 2
     distance = math.sqrt(dist_x + dist_y)
 
-    # if distance > r1 + r2: # 두 원이 떨어져 있을 때
-    #     print(0)
-    # elif distance == r1 + r2: # 외접
-    #     print(1)
-    # elif distance!= 0 and distance == abs(r1 - r2): # 한 원이 다른 원에 내접
-    #     print(1)
-    # elif distance!=0 and distance < r1 + r2: # 내접
-    #     print(2)
-    # elif distance!= 0 and distance < abs(r1 - r2): # 한 원이 다른 원 안에 있음
-    #     print(0)
-    # elif distance == 0 and r1 != r2: # 같은 중심, 다른 반지름
-    #     print(0)
-    # elif distance == 0 and r1 == r2: # 같은 중심, 같은 반지름
-    #     print(-1)
-    if x1 == x2 and y1 == y2:
-        if r1 == r2:
+    if distance > r1 + r2:  # 두 원이 떨어져 있을 때
+        print(0)
+    elif distance == r1 + r2 or (distance != 0 and distance == abs(r1 - r2)):  # 외접 또는 한 원이 다른 원에 내접
+        print(1)
+    elif distance < r1 + r2 and distance > abs(r1 - r2):  # 내접
+        print(2)
+    elif distance == 0:
+        if r1 != r2:  # 같은 중심, 다른 반지름
+            print(0)
+        else:  # 같은 중심, 같은 반지름
             print(-1)
-        else:
-            print(0)
-    else:
-        if r1 + r2 > distance > abs(r1 - r2):
-            print(2)
-        elif r1 + r2 == distance or abs(r1 - r2) == distance:
-            print(1)
-        else:
-            print(0)
+    else:  # 두 원이 한 원이 다른 원 안에 있는 경우
+        print(0)
